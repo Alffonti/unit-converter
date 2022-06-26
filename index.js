4a729c1e3b67cb16681b39f5853f8257bafa2a55
Alf Fonti <alffonti@gmail.com>
2022-06-26 10:54:53 -0300

    update variable names

--- index.js
+++ index.js
@@ -8,18 +8,22 @@ const lengthEl = document.getElementById('length-el')
 const volumeEl = document.getElementById('volume-el')
 const massEl = document.getElementById('mass-el')
 
-convertBtn.addEventListener('click', function convertNumber() {
-    const number = inputEl.value
-    const feet = (number * 3.281).toFixed(2)
-    const meter = (number / 3.281).toFixed(2)
-    const gallon = (number * 0.264).toFixed(2)
-    const liters = (number / 0.264).toFixed(2)
-    const pound = (number * 2.204).toFixed(2)
-    const kilo = (number / 0.264).toFixed(2)
+const meterToFeet =  3.281
+const literToGallon =  0.264
+const kiloToPound =  2.204
 
-    const lengthConversion = `${number} meters = ${feet} feet | ${number} feet = ${meter} meters`
-    const volumeConversion = `${number} liters = ${gallon} gallons | ${number} gallons = ${liters} liters`
-    const massConversion = `${number} kilos = ${pound} pounds | ${number} pounds = ${kilo} kilos`
+convertBtn.addEventListener('click', function convertNumber() {
+    const baseValue = inputEl.value
+    const feet = (baseValue * meterToFeet).toFixed(2) // if the variable was a string, I needed to convert it to a number variable using the Number() method
+    const meter = (baseValue / meterToFeet).toFixed(2)
+    const gallon = (baseValue * literToGallon).toFixed(2)
+    const liters = (baseValue / literToGallon).toFixed(2)
+    const pound = (baseValue * kiloToPound).toFixed(2)
+    const kilo = (baseValue / kiloToPound).toFixed(2)
+
+    const lengthConversion = `${baseValue} meters = ${feet} feet | ${baseValue} feet = ${meter} meters`
+    const volumeConversion = `${baseValue} liters = ${gallon} gallons | ${baseValue} gallons = ${liters} liters`
+    const massConversion = `${baseValue} kilos = ${pound} pounds | ${baseValue} pounds = ${kilo} kilos`
 
     lengthEl.innerHTML = lengthConversion
     volumeEl.innerHTML = volumeConversion

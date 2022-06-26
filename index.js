7bad71fe2d1cccffbff75d621278d2dddbbc211e
Alf Fonti <alffonti@gmail.com>
2022-06-26 12:20:38 -0300

    udpate code

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-// 1 meter = 3.281 feet
-// 1 liter = 0.264 gallon
-// 1 kilogram = 2.204 pound
-
 const inputEl = document.getElementById('input-el')
 const convertBtn = document.getElementById('convert-btn')
 const lengthEl = document.getElementById('length-el')
 const volumeEl = document.getElementById('volume-el')
 const massEl = document.getElementById('mass-el')
 
-const meterToFeet =  3.281
-const literToGallon =  0.264
-const kiloToPound =  2.204
+const meterToFeet =  3.281 // 1 meter = 3.281 feet
+const literToGallon =  0.264 // 1 liter = 0.264 gallon
+const kiloToPound =  2.204 // 1 kilogram = 2.204 pound
 
 convertBtn.addEventListener('click', function convertNumber() {
     const baseValue = inputEl.value
